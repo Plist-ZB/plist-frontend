ad4c07a43ec3c8a1418f8434db73702025423fcb
JIHU96 <korjihu@gmail.com>
2024-12-31 20:14:30 +0900

    chore: 경로 alias 설정 & build 옵션 추가
    - build 시 모든 콘솔출력과 디버거 구문 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,22 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  resolve: {
+    alias: [
+      { find: "public", replacement: "/public" },
+      { find: "@", replacement: "/src" },
+    ],
+  },
   server: {
     port: 3000,
+  },
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
 });

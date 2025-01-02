2f7a5f0df5f372fa4dfe4610014c01f92245a038
You-NaSong <twins7534@naver.com>
2025-01-02 19:42:31 +0900

    feat : Welcome 페이지 style

--- src/pages/auth/WelcomePage.tsx
+++ src/pages/auth/WelcomePage.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import LogoIcon from "@/assets/logo.png";
 
 export default function WelcomePage() {
@@ -15,3 +16,50 @@ export default function WelcomePage() {
     </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: white;
+`;
+
+const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+  height: 80px;
+  margin-bottom: 8px;
+`;
+
+const LogoText = styled.h1`
+  font-size: 24px;
+  color: #4654a3;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const MessageWrapper = styled.div`
+  text-align: center;
+`;
+
+const WelcomeText = styled.h2`
+  font-size: 30px;
+  font-weight: bold;
+  color: #333;
+  margin: 0;
+  margin-bottom: 8px;
+`;
+
+const SubText = styled.p`
+  font-size: 20px;
+  color: #666;
+  margin: 0;
+`;

0ee09b770f9266d3d04de2268dd32675006e17b3
You-NaSong <twins7534@naver.com>
2025-01-02 19:42:01 +0900

    feat : Welcome 페이지 JSX

--- src/pages/auth/WelcomePage.tsx
+++ src/pages/auth/WelcomePage.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import LogoIcon from "@/assets/logo.png";
 
 export default function WelcomePage() {
-  return <div>WelcomePage</div>;
+  return (
+    <Container>
+      <LogoWrapper>
+        <Logo src={LogoIcon} alt="Logo" />
+        <LogoText>PLIST</LogoText>
+      </LogoWrapper>
+      <MessageWrapper>
+        <WelcomeText>가입을 환영합니다!</WelcomeText>
+        <SubText>플리스트와 함께 즐거운 음악을 공유해요!</SubText>
+      </MessageWrapper>
+    </Container>
+  );
 }

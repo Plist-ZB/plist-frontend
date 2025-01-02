65e163daa1683b395e13d0a320992f44bd68e2aa
You-NaSong <twins7534@naver.com>
2025-01-02 19:24:13 +0900

    feat : 로딩 JSX

--- src/pages/auth/RedirectPage.tsx
+++ src/pages/auth/RedirectPage.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import LogoIcon from "@/assets/logo.png";
+import { ScaleLoader } from "react-spinners";
 
 export default function RedirectPage() {
   return <div>RedirectPage</div>;
+  return (
+    <Container>
+      <LogoWrapper>
+        <Logo src={LogoIcon} alt="Logo" />
+        <LogoText>PLIST</LogoText>
+      </LogoWrapper>
+      <Spinners>
+        <ScaleLoader
+          color="#4654A3"
+          height={40}
+          margin={2}
+          radius={0}
+          speedMultiplier={0.7}
+          width={6}
+        />
+      </Spinners>
+    </Container>
+  );
 }

bc7fa01f401964dd749ecb80d3df825bf3839fc6
You-NaSong <twins7534@naver.com>
2025-01-02 19:50:08 +0900

    feat : 페이지 이동 문구 추가
    "3초뒤 자동으로 메인화면으로 이동합니다."

--- src/pages/auth/WelcomePage.tsx
+++ src/pages/auth/WelcomePage.tsx
@@ -7,12 +7,12 @@ export default function WelcomePage() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // 3초 후 메인 화면(/main)으로 이동
+    //3초 후 메인 화면(/main)으로 이동
     const timer = setTimeout(() => {
       navigate("/HomePage");
     }, 3000);
 
-    // 컴포넌트가 언마운트되면 타이머를 클리어
+    //컴포넌트가 언마운트되면 타이머를 클리어
     return () => clearTimeout(timer);
   }, [navigate]);
 
@@ -25,6 +25,7 @@ export default function WelcomePage() {
       <MessageWrapper>
         <WelcomeText>가입을 환영합니다!</WelcomeText>
         <SubText>플리스트와 함께 즐거운 음악을 공유해요!</SubText>
+        <SubText1>3초뒤 자동으로 메인화면으로 이동합니다.</SubText1>
       </MessageWrapper>
     </Container>
   );
@@ -75,4 +76,10 @@ const SubText = styled.p`
   font-size: 20px;
   color: #666;
   margin: 0;
+`;
+
+const SubText1 = styled.p`
+  font-size: 15px;
+  color: #4654a3;
+  margin: 10px;
 `;

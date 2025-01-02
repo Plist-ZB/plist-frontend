5c40ec41f7f3f9ed6fcfcb6d9a341f0241bededf
You-NaSong <twins7534@naver.com>
2025-01-02 18:03:36 +0900

    feat : 로그인 화면 JSX
    1. 구글 로그인
    2. 카카오 로그인 btn 주석처리
    3. 로고 아직 미정

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <Container>
+      <LogoWrapper>
+        <Logo src={LogoIcon} alt="Logo" />
+        <Title>PLIST</Title>
+      </LogoWrapper>
+      <ButtonWrapper>
+        <LoginButton>
+          <Icon>🔒</Icon>
+          Google 로그인하기
+        </LoginButton>
+        {/*<LoginButton>
+          <Icon>🔒</Icon>
+          카카오 로그인하기
+        </LoginButton>*/}
+      </ButtonWrapper>
+    </Container>
+  );
 }

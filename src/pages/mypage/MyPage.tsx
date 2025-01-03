6fb24265beb7d94b2ef04f47de9c977861e51f86
JIHU96 <korjihu@gmail.com>
2025-01-03 20:00:18 +0900

    feat: 마이페이지 UI 구현 [#11]

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import UserInfo from "./components/mypage/UserInfo";
+import MenuButton from "./components/mypage/MenuButton";
 
 export default function MyPage() {
-  return <div>MyPage</div>;
+  const mockUser = {
+    name: "김플리",
+    email: "test@test.com",
+    profileImage: undefined,
+  };
+
+  const myPageMenus = [
+    {
+      name: "내 플레이리스트",
+      to: "/mypage/playlist",
+    },
+    {
+      name: "내 호스트 이력 보기",
+      to: "/mypage/host-history",
+    },
+  ];
+
+  const onClickLogout = () => {
+    console.log("로그아웃");
+  };
+
+  return (
+    <div className="flex flex-col w-full h-full">
+      <header className="flex items-center justify-end w-full px-4 h-header">
+        <button className="text-gray-400" onClick={onClickLogout}>
+          로그아웃
+        </button>
+      </header>
+
+      <UserInfo user={mockUser} />
+
+      <div className="flex flex-col w-full gap-6 px-4 py-8">
+        {myPageMenus.map((menu) => (
+          <MenuButton key={menu.name} name={menu.name} to={menu.to} />
+        ))}
+      </div>
+    </div>
+  );
 }

0e11e12adbc1ce325cb97a057dc5db980a2d7e50
You-NaSong <twins7534@naver.com>
2025-01-03 14:41:04 +0900

    feat : 메인화면 JSX

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import Header from "../../layout/Header";
+import FooterNavBar from "../../layout/FooterNavBar";
+import HeaderButton from "../../layout/HeaderButton";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <Container>
+      <Header />
+      <MainContent>
+        <CategoryButtons>
+          <CategoryButton>최근</CategoryButton>
+          <CategoryButton>인기</CategoryButton>
+          <CategoryButtonRight>카테고리</CategoryButtonRight>
+        </CategoryButtons>
+        <StreamList>
+          {streams.map((stream, index) => (
+            <StreamCard key={index}>
+              <LiveBadge>Live</LiveBadge>
+              <Thumbnail>썸네일</Thumbnail>
+              <StreamDetails>
+                <Title>{stream.title}</Title>
+                <Info>{stream.host}</Info>
+                <Info>카테고리: {stream.category}</Info>
+                <Info>스트리밍 시간: {stream.duration}</Info>
+              </StreamDetails>
+            </StreamCard>
+          ))}
+        </StreamList>
+      </MainContent>
+      <FooterNavBar />
+    </Container>
+  );
 }

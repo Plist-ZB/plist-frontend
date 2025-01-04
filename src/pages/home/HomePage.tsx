eea39d4261f033b16284a90ba02a79d458ce61eb
You-NaSong <twins7534@naver.com>
2025-01-04 17:31:45 +0900

    fix : 컴포넌트 재사용
    채널리스트 컴포넌트 사용으로 코드 수정

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "../../layout/Header";
 import FooterNavBar from "../../layout/FooterNavBar";
+import StreamList from "@/common/components/StreamList";
 
 export default function HomePage() {
   // 카테고리 토글바 구현현
@@ -27,44 +28,12 @@ export default function HomePage() {
             </ToggleBar>
           </CategoryButtonRight>
         </CategoryButtons>
-        <StreamList>
-          {streams.map((stream, index) => (
-            <StreamCard key={index}>
-              <Thumbnail>
-                <LiveBadge>Live</LiveBadge>
-                썸네일
-              </Thumbnail>
-              <StreamDetails>
-                <Title>
-                  <b>{stream.title}</b>
-                </Title>
-                <Info>{stream.host}</Info>
-                <Info>카테고리: {stream.category}</Info>
-                <Info>스트리밍 시간: {stream.duration}</Info>
-              </StreamDetails>
-            </StreamCard>
-          ))}
-        </StreamList>
+        <StreamList />
       </MainContent>
       <FooterNavBar />
     </Container>
   );
 }
-
-const streams = [
-  {
-    title: "발라드 추천 좀 해주세요~",
-    host: "노지훈",
-    category: "발라드, 가을",
-    duration: "1시간 27분",
-  },
-  {
-    title: "데이식스 전곡 듣기",
-    host: "송유나",
-    category: "밴드",
-    duration: "1시간 27분",
-  },
-];
 
 const Container = styled.div`
   display: flex;
@@ -115,62 +84,4 @@ const ToggleBar = styled.div`
   max-height: ${(props) => (props.isOpen ? "300px" : "0")}; /* isOpen 상태에 따라 높이 설정 */
   overflow: hidden; /* 내용이 넘치지 않게 숨김 */
   transition: max-height 0.3s ease-in-out; /* 부드러운 애니메이션 */
-`;
-
-const StreamList = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  border-radius: 8px;
-`;
-
-const StreamCard = styled.div`
-  display: flex;
-  gap: 16px;
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 16px;
-`;
-
-//Live 뱃지
-const LiveBadge = styled.div`
-  position: absolute;
-  top: 4px;
-  left: 4px;
-  background: red;
-  color: white;
-  padding: 2px 4px;
-  border-radius: 8px;
-  font-size: 12px;
-  font-weight: regule;
-`;
-
-const Thumbnail = styled.div`
-  position: relative;
-  width: 100px;
-  height: 100px;
-  background: var(--color-primary-50);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 8px;
-  font-size: 14px;
-  color: black;
-`;
-
-const StreamDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const Title = styled.h3`
-  font-size: 16px;
-  margin: 0;
-`;
-
-const Info = styled.p`
-  margin: 4px 0;
-  font-size: 14px;
-  color: #555;
 `;
--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Header from "../../layout/Header";
 import FooterNavBar from "../../layout/FooterNavBar";
 import { IoIosSearch } from "react-icons/io";
+import StreamList from "@/common/components/StreamList";
 
 interface SearchResult {
   id: number;
@@ -48,24 +49,7 @@ export default function SearchPage() {
         {loading ? (
           <LoadingText>검색 중...</LoadingText>
         ) : results === null ? null : results.length > 0 ? ( // 검색을 수행하지 않았을 경우 아무것도 표시하지 않음
-          <StreamList>
-            {streams.map((stream, index) => (
-              <StreamCard key={index}>
-                <Thumbnail>
-                  <LiveBadge>Live</LiveBadge>
-                  썸네일
-                </Thumbnail>
-                <StreamDetails>
-                  <Title>
-                    <b>{stream.title}</b>
-                  </Title>
-                  <Info>{stream.host}</Info>
-                  <Info>카테고리: {stream.category}</Info>
-                  <Info>스트리밍 시간: {stream.duration}</Info>
-                </StreamDetails>
-              </StreamCard>
-            ))}
-          </StreamList>
+          <StreamList />
         ) : (
           <ResultText>검색 결과가 없습니다.</ResultText>
         )}
@@ -126,77 +110,4 @@ const ResultText = styled.p`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-// 채널 리스트
-const streams = [
-  {
-    title: "발라드 추천 좀 해주세요~",
-    host: "노지훈",
-    category: "발라드, 가을",
-    duration: "1시간 27분",
-  },
-  {
-    title: "데이식스 전곡 듣기",
-    host: "송유나",
-    category: "밴드",
-    duration: "1시간 27분",
-  },
-];
-
-const StreamList = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  border-radius: 8px;
-`;
-
-const StreamCard = styled.div`
-  display: flex;
-  gap: 16px;
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  padding: 16px;
-`;
-
-const LiveBadge = styled.div`
-  position: absolute;
-  top: 4px;
-  left: 4px;
-  background: red;
-  color: white;
-  padding: 2px 4px;
-  border-radius: 8px;
-  font-size: 12px;
-  font-weight: regule;
-`;
-
-const Thumbnail = styled.div`
-  position: relative;
-  width: 100px;
-  height: 100px;
-  background: var(--color-primary-50);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 8px;
-  font-size: 14px;
-  color: black;
-`;
-
-const StreamDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-const Title = styled.h3`
-  font-size: 16px;
-  margin: 0;
-`;
-
-const Info = styled.p`
-  margin: 4px 0;
-  font-size: 14px;
-  color: #555;
 `;

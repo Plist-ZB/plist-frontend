7619cb9c7039a7e438b44dadc49ab81cf378ce92
You-NaSong <twins7534@naver.com>
2025-01-04 15:35:57 +0900

    style : 검색 후 채널리스트 style 구현

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -83,3 +83,76 @@ const ResultText = styled.p`
   justify-content: center;
   align-items: center;
 `;
+
+// 채널 리스트
+const streams = [
+  {
+    title: "발라드 추천 좀 해주세요~",
+    host: "노지훈",
+    category: "발라드, 가을",
+    duration: "1시간 27분",
+  },
+  {
+    title: "데이식스 전곡 듣기",
+    host: "송유나",
+    category: "밴드",
+    duration: "1시간 27분",
+  },
+];
+
+const StreamList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  border-radius: 8px;
+`;
+
+const StreamCard = styled.div`
+  display: flex;
+  gap: 16px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+`;
+
+const LiveBadge = styled.div`
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  background: red;
+  color: white;
+  padding: 2px 4px;
+  border-radius: 8px;
+  font-size: 12px;
+  font-weight: regule;
+`;
+
+const Thumbnail = styled.div`
+  position: relative;
+  width: 100px;
+  height: 100px;
+  background: var(--color-primary-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  font-size: 14px;
+  color: black;
+`;
+
+const StreamDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Title = styled.h3`
+  font-size: 16px;
+  margin: 0;
+`;
+
+const Info = styled.p`
+  margin: 4px 0;
+  font-size: 14px;
+  color: #555;
+`;

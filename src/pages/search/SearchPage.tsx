cd6fb30b0df8e363c6ef00685dae32f9ef88497e
You-NaSong <twins7534@naver.com>
2025-01-04 15:16:50 +0900

    feat : 검색  페이지 JSX 구현

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Header from "../../layout/Header";
+import FooterNavBar from "../../layout/FooterNavBar";
 
 export default function SearchPage() {
-  return <div>SearchPage</div>;
+  return (
+    <Container>
+      <Header />
+      <MainContent>
+        <SearchBarContainer>
+          <SearchInput placeholder="검색창" />
+        </SearchBarContainer>
+        <Line />
+        <ResultText>검색 결과가 없습니다.</ResultText>
+      </MainContent>
+      <FooterNavBar />
+    </Container>
+  );
 }

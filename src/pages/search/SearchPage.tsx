0e902cd272a8908a7cb43a16da566df4c13e37b7
You-NaSong <twins7534@naver.com>
2025-01-04 15:17:31 +0900

    feat : 검색 페이지 style 구현
    react-icons 사용해서 검색 아이콘 구현

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import styled from "styled-components";
 import Header from "../../layout/Header";
 import FooterNavBar from "../../layout/FooterNavBar";
+import { IoIosSearch } from "react-icons/io";
 
 export default function SearchPage() {
   return (
@@ -9,6 +11,7 @@ export default function SearchPage() {
       <MainContent>
         <SearchBarContainer>
           <SearchInput placeholder="검색창" />
+          <IoIosSearch size={20} color="#888787" />
         </SearchBarContainer>
         <Line />
         <ResultText>검색 결과가 없습니다.</ResultText>
@@ -17,3 +20,48 @@ export default function SearchPage() {
     </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  padding: 16px;
+  overflow-y: auto;
+`;
+
+const SearchBarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 90%;
+  margin: 20px 35px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 8px;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #ddd;
+  margin-top: 35px;
+`;
+
+const SearchInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  font-size: 16px;
+`;
+
+const ResultText = styled.p`
+  margin-top: 30px;
+  font-size: 15px;
+  color: #888;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

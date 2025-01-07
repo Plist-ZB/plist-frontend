f0ef57fbb4b8dff081eea69ce7085f34fcd387f8
You-NaSong <twins7534@naver.com>
2025-01-07 20:54:13 +0900

    feat : CategoryPage JSX구현

--- src/pages/category/CategoryPage.tsx
+++ src/pages/category/CategoryPage.tsx
@@ -1,3 +1,14 @@
+import React from "react";
+
 export default function CategoryPage() {
   return <div>CategoryPage</div>;
+  return (
+    <Container>
+      {categories.map((category, index) => (
+        <CategoryButton key={index} onClick={() => handleCategoryClick(category)}>
+          {category}
+        </CategoryButton>
+      ))}
+    </Container>
+  );
 }

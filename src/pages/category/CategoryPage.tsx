640fef4e31089803290338558b81415feefba63e
JIHU96 <korjihu@gmail.com>
2025-01-08 00:46:03 +0900

    fix: 카테고리 페이지의 버튼 사이즈 수정

--- src/pages/category/CategoryPage.tsx
+++ src/pages/category/CategoryPage.tsx
@@ -1,9 +1,29 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 export default function CategoryPage() {
-  const categories = ["공부", "힙합", "K-POP", "POP", "봄", "여름", "가을", "겨울"];
+  const categories = [
+    "공부",
+    "힙합",
+    "K-POP",
+    "POP",
+    "봄",
+    "여름",
+    "가을",
+    "겨울",
+    "공부",
+    "힙합",
+    "K-POP",
+    "POP",
+    "봄",
+    "여름",
+    "가을",
+    "겨울",
+    "공부",
+    "힙합",
+    "K-POP",
+    "POP",
+  ];
   const navigate = useNavigate(); // React Router의 useNavigate 사용
 
   const handleCategoryClick = (category: string) => {
@@ -26,17 +46,18 @@ const Container = styled.div`
   grid-template-columns: repeat(2, 1fr);
   gap: 16px;
   width: 100%;
-  height: 600px;
-  max-width: 1200px;
-  margin: 0 auto;
+  height: 100%;
+  overflow-y: auto;
   padding: 20px;
 `;
 
 const CategoryButton = styled.button`
+  width: 100%;
+  height: 100%;
   background-color: white;
   border: 1px solid #ccc;
   border-radius: 8px;
-  padding: 16px;
+  padding: 32px;
   font-size: 16px;
   cursor: pointer;
   transition: border 0.3s ease;

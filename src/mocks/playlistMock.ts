b4e67e487c0d86b145a1f6c264844f659e415d82
JIHU96 <korjihu@gmail.com>
2025-01-09 20:23:00 +0900

    feat: 플레이리스트 목데이터 추가

---
+++ src/mocks/playlistMock.ts
@@ -0,0 +1,123 @@
+export const mockData = [
+  {
+    id: 0,
+    videoId: "Mhyi9p7T7OM",
+    videoName: "예뻤어 You Were Beautiful",
+    videoThumbnail: "https://i.ytimg.com/vi/Mhyi9p7T7OM/mqdefault.jpg",
+    videoDescription:
+      "Provided to YouTube by JYP Entertainment Corporation 예뻤어 You Were Beautiful · DAY6 Every DAY6 February ℗ 2017 JYP ...",
+  },
+  {
+    id: 1,
+    videoId: "BS7tz2rAOSA",
+    videoName: "DAY6 &quot;You Were Beautiful(예뻤어)&quot; M/V",
+    videoThumbnail: "https://i.ytimg.com/vi/BS7tz2rAOSA/mqdefault.jpg",
+    videoDescription:
+      'DAY6(데이식스) "You Were Beautiful(예뻤어)" M/V Listen to DAY6 "Every DAY6 February" on iTunes & Apple Music: ...',
+  },
+  {
+    id: 2,
+    videoId: "siF3GM68IDE",
+    videoName:
+      "DAY6 &quot;You Were Beautiful(예뻤어) &amp; Congratulations&quot;_ DAY6 2ND WORLD TOUR &#39;GRAVITY&#39;",
+    videoThumbnail: "https://i.ytimg.com/vi/siF3GM68IDE/mqdefault.jpg",
+    videoDescription:
+      'DAY6 "You Were Beautiful(예뻤어) & Congratulations"_ DAY6 2ND WORLD TOUR \'GRAVITY\' Listen to DAY6 "The Book of Us ...',
+  },
+  {
+    id: 3,
+    videoId: "Ejc__vhP2U8",
+    videoName: "DAY6(데이식스)- 예뻤어 [가사/Lyrics]",
+    videoThumbnail: "https://i.ytimg.com/vi/Ejc__vhP2U8/mqdefault.jpg",
+    videoDescription:
+      "DAY6(데이식스)- 예뻤어 [가사/Lyrics] #DAY6 #데이식스 #예뻤어 발매 : 2017.02.06 아티스트 : DAY6(데이식스) 앨범 : Every DAY6 ...",
+  },
+  {
+    id: 4,
+    videoId: "QMgEsc4hbpE",
+    videoName: "DAY6 - You Were Beautiful (예뻤어) Lyrics [HAN|ROM|ENG]",
+    videoThumbnail: "https://i.ytimg.com/vi/QMgEsc4hbpE/mqdefault.jpg",
+    videoDescription: "",
+  },
+  {
+    id: 5,
+    videoId: "C4bUrP_Pkfs",
+    videoName:
+      "DAY6 performs &quot;You Were Beautiful (예뻤어)&quot; &amp; &quot;Time of Our Life (한 페이지가 될 수 있게)&quot; 🎸🥁💥",
+    videoThumbnail: "https://i.ytimg.com/vi/C4bUrP_Pkfs/mqdefault.jpg",
+    videoDescription:
+      'DAY6 performing "You Were Beautiful (예뻤어)" & "Time of our Life (한 페이지가 될 수 있게)" live in studio. Congrats to DAY6 on ...',
+  },
+  {
+    id: 6,
+    videoId: "D4A5mYjXHVA",
+    videoName: "DAY6 - 예뻤어 (You Were Beautiful) (audio)",
+    videoThumbnail: "https://i.ytimg.com/vi/D4A5mYjXHVA/mqdefault.jpg",
+    videoDescription:
+      "Album: Every DAY6 February Release Date: February 6, 2017 DISCLAIMER: I do not own the audio or the song in this video.",
+  },
+  {
+    id: 7,
+    videoId: "JUuCHXNbeyk",
+    videoName:
+      "데이식스(Day6) - 예뻤어 를 콘서트 홀에서 듣는다면? / 공간음향으로 들어보는 데이식스 - 예뻤어",
+    videoThumbnail: "https://i.ytimg.com/vi/JUuCHXNbeyk/mqdefault.jpg",
+    videoDescription:
+      "듣고싶어서 하나둘씩 채워가는 공간음향 플리입니다. ✔️이어폰/헤드폰을 착용하고 들어주시면 콘서트 홀 같은 공간감을 느낄 수 ...",
+  },
+  {
+    id: 8,
+    videoId: "IU3zOXqzBQs",
+    videoName: "[⏳1절노래] 예뻤어_데이식스 *MV",
+    videoThumbnail: "https://i.ytimg.com/vi/IU3zOXqzBQs/mqdefault.jpg",
+    videoDescription:
+      "DAY6Official DAY6 디지털 싱글 Every DAY6 February 1번 트랙 예뻤어 1절 요약 공식 오디오: ...",
+  },
+  {
+    id: 9,
+    videoId: "r72FVsmMVyU",
+    videoName: "[🪄사비깎] 예뻤어_데이식스 *MV #shorts",
+    videoThumbnail: "https://i.ytimg.com/vi/r72FVsmMVyU/mqdefault.jpg",
+    videoDescription:
+      "DAY6 디지털 싱글 Every DAY6 February 1번 트랙 예뻤어 사비 요약 공식 오디오: www.youtube.com/watch?v=Mhyi9p7T7OM 1절: ...",
+  },
+  {
+    id: 10,
+    videoId: "QIPtekqRnFc",
+    videoName: "Space Oddity - 예쁘게 예뻤어(Official Audio)",
+    videoThumbnail: "https://i.ytimg.com/vi/QIPtekqRnFc/mqdefault.jpg",
+    videoDescription:
+      "Official audio by Space Oddity performing “예쁘게 예뻤어” – Debut EP album 'Lucky 7 EP' Out Now 8/11/2019 Genie ...",
+  },
+  {
+    id: 11,
+    videoId: "g83xaZiMqbs",
+    videoName:
+      "Day6 - You Were Beautiful (예뻤어) (Eng &amp; Korean ver.) Left &amp; Right ear [USE HEADPHONES] (Split Audio)",
+    videoThumbnail: "https://i.ytimg.com/vi/g83xaZiMqbs/mqdefault.jpg",
+    videoDescription:
+      'Video clip used: DAY6 "You Were Beautiful(예뻤어)" M/V - https://youtu.be/BS7tz2rAOSA Songs used: You Were Beautiful You ...',
+  },
+  {
+    id: 12,
+    videoId: "8AcfWilJLsY",
+    videoName: "[✂️1절with브릿지] 예뻤어_데이식스 *MV",
+    videoThumbnail: "https://i.ytimg.com/vi/8AcfWilJLsY/mqdefault.jpg",
+    videoDescription:
+      "DAY6Official DAY6 디지털 싱글 Every DAY6 February 1번 트랙 예뻤어 1절 with 브릿지 요약 공식 오디오: ...",
+  },
+  {
+    id: 13,
+    videoId: "3_ZflnWz7iw",
+    videoName: "예뻤어 (You Were Beautiful) (Short Ver.1)-DAY6 (Official Audio)",
+    videoThumbnail: "https://i.ytimg.com/vi/3_ZflnWz7iw/mqdefault.jpg",
+    videoDescription: "",
+  },
+  {
+    id: 14,
+    videoId: "UBbf9DLn-KI",
+    videoName: "예뻤어 (You Were Beautiful) (Short Ver.2)-DAY6 (Official Audio)",
+    videoThumbnail: "https://i.ytimg.com/vi/UBbf9DLn-KI/mqdefault.jpg",
+    videoDescription: "",
+  },
+];

ea3c2f27341c57db1f21d8427fa2b4fdfc5b72d9
JIHU96 <korjihu@gmail.com>
2025-01-09 16:41:12 +0900

    fix: 스트리밍 중인 카드 컴포넌트 디자인 변경

--- src/common/components/StreamCard.tsx
+++ src/common/components/StreamCard.tsx
@@ -1,20 +1,31 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
 
 interface StreamCardProps {
   readonly item: {
+    id: number;
     title: string;
     host: string;
     category: string;
     duration: string;
+    participants: number;
   };
-  readonly onClick: () => void;
 }
 
-export default function StreamCard({ item, onClick }: StreamCardProps) {
+/* 
+최근: 스트리밍 중인 전체 항목 100개를 다 줌
+
+*/
+
+export default function StreamCard({ item }: StreamCardProps) {
   return (
-    <Card onClick={onClick}>
+    <Card to={`/stream/${item.id}`}>
       <Thumbnail>
-        <LiveBadge>Live</LiveBadge>
+        <LiveBadge>
+          <FaUserAlt size={8} />
+          {item.participants}
+        </LiveBadge>
         썸네일
       </Thumbnail>
       <StreamDetails>
@@ -29,24 +40,30 @@ export default function StreamCard({ item, onClick }: StreamCardProps) {
   );
 }
 
-const Card = styled.button`
+const Card = styled(Link)`
   display: flex;
   gap: 16px;
   border: 1px solid #ddd;
   border-radius: 8px;
   padding: 16px;
+
+  &:hover {
+    border: 1px solid var(--color-primary);
+  }
 `;
 
 const LiveBadge = styled.div`
   position: absolute;
-  top: 4px;
-  left: 4px;
-  background: red;
-  color: white;
+  top: 6px;
+  left: 6px;
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  color: var(--color-white);
+  background-color: var(--color-red);
+  font-size: 10px;
   padding: 2px 4px;
   border-radius: 8px;
-  font-size: 12px;
-  font-weight: bold;
 `;
 
 const Thumbnail = styled.div`
@@ -72,6 +89,10 @@ const StreamDetails = styled.div`
 const Title = styled.h3`
   font-size: 16px;
   margin: 0;
+
+  b {
+    color: var(--color-black);
+  }
 `;
 
 const Info = styled.p`

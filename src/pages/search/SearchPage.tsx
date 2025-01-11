ba0ca0f90a9441584ba3aa74b6689d4f3195581e
JIHU96 <korjihu@gmail.com>
2025-01-11 19:21:18 +0900

    fix: 타입 강화 & 스타일 부분 수정

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -5,11 +5,14 @@ import StreamCard from "@/common/components/StreamCard";
 import { instance } from "@/services/api/instance";
 
 interface SearchResult {
-  id: number;
+  channelId: number;
   channelName: string;
-  channelHost: string;
   channelCategoryName: string;
-  description: string;
+  channelThumbnail: string;
+  channelCreatedAt: string;
+  channelStatus: string;
+  channelHost: string;
+  channelParticipantCount: number;
 }
 
 export default function SearchPage() {
@@ -22,8 +25,9 @@ export default function SearchPage() {
     setLoading(true);
     try {
       // API 호출: instance 사용 (이때 DB로부터 검색된 채널 정보 받아옴)
-      const response = await instance.get<SearchResult[]>("/search", {
-        params: { query: searchQuery }, // 쿼리 파라미터로 검색어 전달
+      const response = await instance.get<SearchResult[]>(`/search`, {
+        // TODO: json-server 맞춤이라 msw 변경이나 실제 API로 변경 필요
+        params: { channelCategoryName: searchQuery },
       });
 
       // 받아온 데이터에서 StreamCard에 필요한 항목 추출
@@ -46,32 +50,32 @@ export default function SearchPage() {
 
   return (
     <Container>
-      <MainContent>
-        <SearchBarContainer>
-          <SearchInput
-            placeholder="채널명, 카테고리, 호스트명을 검색해주세요."
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            onKeyDown={handleKeyDown} // Enter 키 이벤트 핸들러 추가
-          />
-          <SearchIcon onClick={handleSearch}>
-            <IoIosSearch onClick={handleSearch} size={20} color="#888787" />
-          </SearchIcon>
-        </SearchBarContainer>
-        <Line />
-        {/* 검색 결과 */}
-        {loading ? (
-          <LoadingText>로딩 중...</LoadingText>
-        ) : noResults ? (
-          <ResultText>검색 결과가 없습니다.</ResultText> // 검색 결과가 없을 경우 메시지
-        ) : (
-          <ResultsContainer>
-            {results?.map((result) => (
-              <StreamCard key={result.id} item={result} /> // 검색된 채널을 StreamCard에 전달
-            ))}
-          </ResultsContainer>
-        )}
-      </MainContent>
+      <SearchBarContainer>
+        <SearchInput
+          placeholder="채널명, 카테고리, 호스트명을 검색해주세요."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyDown={handleKeyDown} // Enter 키 이벤트 핸들러 추가
+        />
+        <SearchIcon onClick={handleSearch}>
+          <IoIosSearch onClick={handleSearch} size={20} color="#888787" />
+        </SearchIcon>
+      </SearchBarContainer>
+
+      <Line />
+
+      {/* 검색 결과 */}
+      {loading ? (
+        <LoadingText>로딩 중...</LoadingText>
+      ) : noResults ? (
+        <ResultText>검색 결과가 없습니다.</ResultText> // 검색 결과가 없을 경우 메시지
+      ) : (
+        <ResultsContainer>
+          {results?.map((result) => (
+            <StreamCard key={result.channelId} item={result} /> // 검색된 채널을 StreamCard에 전달
+          ))}
+        </ResultsContainer>
+      )}
     </Container>
   );
 }
@@ -79,20 +83,15 @@ export default function SearchPage() {
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
-`;
-
-const MainContent = styled.main`
-  flex: 1;
-  padding: 16px;
-  overflow-y: auto;
+  padding: 24px 16px;
+  gap: 24px;
 `;
 
 const SearchBarContainer = styled.div`
   display: flex;
+  justify-content: center;
   align-items: center;
-  width: 90%;
-  margin: 20px 35px;
+  width: 100%;
   border: 1px solid #ddd;
   border-radius: 8px;
   padding: 8px;
@@ -103,6 +102,13 @@ const SearchIcon = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #ddd;
+  //margin: 16px 0 0 0;
 `;
 
 const ResultsContainer = styled.div`
@@ -116,13 +122,6 @@ const LoadingText = styled.p`
   color: #888;
 `;
 
-const Line = styled.div`
-  width: 100%;
-  height: 1px;
-  background-color: #ddd;
-  margin-top: 35px;
-`;
-
 const SearchInput = styled.input`
   flex: 1;
   border: none;
@@ -132,7 +131,7 @@ const SearchInput = styled.input`
 `;
 
 const ResultText = styled.p`
-  margin-top: 30px;
+  margin-top: 16px;
   font-size: 15px;
   color: #888;
   display: flex;

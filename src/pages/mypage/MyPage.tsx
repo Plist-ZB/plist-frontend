8b157203b72543e551ddd7bf5e77c7a07d2fa192
JIHU96 <korjihu@gmail.com>
2025-01-11 17:43:40 +0900

    fix: USerInfo 컴포넌트 props 제거 & 로그아웃 버튼과 프로필변경 버튼 위치 변경

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,37 +1,37 @@
 import UserInfo from "./components/mypage/UserInfo";
 import MenuButton from "./components/mypage/MenuButton";
+import { Link } from "react-router-dom";
+import { UserRoundPen } from "lucide-react";
+
+const myPageMenus = [
+  {
+    name: "내 플레이리스트",
+    to: "/mypage/playlist",
+  },
+  {
+    name: "내 호스트 이력 보기",
+    to: "/mypage/host-history",
+  },
+];
 
 export default function MyPage() {
-  const mockUser = {
-    name: "김플리",
-    email: "test@test.com",
-    profileImage: undefined,
-  };
-
-  const myPageMenus = [
-    {
-      name: "내 플레이리스트",
-      to: "/mypage/playlist",
-    },
-    {
-      name: "내 호스트 이력 보기",
-      to: "/mypage/host-history",
-    },
-  ];
-
   const onClickLogout = () => {
     console.log("로그아웃");
   };
 
   return (
     <div className="flex flex-col w-full h-full">
-      <header className="flex items-center justify-end w-full px-4 h-header">
-        <button className="text-gray-400" onClick={onClickLogout}>
+      <header className="flex items-center justify-between w-full px-4 h-header">
+        <button className="p-2 text-gray-light" onClick={onClickLogout}>
           로그아웃
         </button>
+
+        <Link to="/mypage/profile" className="p-2 text-gray-600" aria-label="프로필 수정">
+          <UserRoundPen />
+        </Link>
       </header>
 
-      <UserInfo user={mockUser} />
+      <UserInfo />
 
       <div className="flex flex-col w-full gap-6 px-4 py-8">
         {myPageMenus.map((menu) => (

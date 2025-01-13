9f7a2f98d334762cc34417d227dd825bcd8b4a93
JIHU96 <korjihu@gmail.com>
2025-01-13 22:57:01 +0900

    feat: UserInfo 컴포넌트에 백그라운드 이미지 프로필 사진 추가

--- src/pages/mypage/components/mypage/UserInfo.tsx
+++ src/pages/mypage/components/mypage/UserInfo.tsx
@@ -6,9 +6,10 @@ export default function UserInfo() {
   return (
     <div className="flex flex-col items-center w-full pb-4 border-b border-gray-200">
       <div
-        className={`border rounded-full w-28 h-28 border-gray-border ${
+        className={`bg-center bg-cover border rounded-full w-28 h-28 border-gray-border ${
           isProfileLoading ? "animate-pulse bg-gray-border" : "bg-primary-50"
         }`}
+        style={{ backgroundImage: `url('${userProfile?.image}')` }}
       ></div>
 
       <div className="flex items-center justify-center w-full gap-2 mt-4 mb-1 text-base font-bold">
@@ -17,7 +18,7 @@ export default function UserInfo() {
             isProfileLoading ? "w-14 animate-pulse bg-gray-border" : ""
           }`}
         >
-          {!isProfileLoading && userProfile?.user_name}
+          {!isProfileLoading && userProfile?.nickname}
         </div>
       </div>
 
@@ -26,7 +27,7 @@ export default function UserInfo() {
           isProfileLoading ? "w-10 bg-gray-border animate-pulse" : ""
         }`}
       >
-        {!isProfileLoading && userProfile?.user_email}
+        {!isProfileLoading && userProfile?.email}
       </div>
     </div>
   );

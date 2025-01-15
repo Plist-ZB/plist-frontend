919ec29891bef1f748c811b61003ceb2dc4f2637
JIHU96 <korjihu@gmail.com>
2025-01-15 13:49:09 +0900

    feat: 내 재생목록 페이지에 API 로직 연결

--- src/pages/mypage/PlaylistPage.tsx
+++ src/pages/mypage/PlaylistPage.tsx
@@ -1,41 +1,13 @@
 import TopBarLayout from "@/layout/TopBarLayout";
 import CreatePlaylistButton from "@/pages/mypage/components/playlist/CreatePlaylistButton";
 import PlaylistItem from "@/pages/mypage/components/playlist/PlaylistItem";
+import PlaylistSkeleton from "@/pages/mypage/components/playlist/PlaylistSkeleton";
+import usePlaylist from "@/pages/mypage/hooks/usePlaylist";
+import { useDebounce } from "@/common/hooks/useDebounce";
 
 export default function PlaylistPage() {
-  // TODO: 플레이리스트 데이터 받아오기
-  const mock = [
-    {
-      id: 1,
-      thumbnails: "https://picsum.photos/100/100",
-      title: "플레이리스트1",
-      track_count: 10,
-    },
-    {
-      id: 2,
-      thumbnails: "https://picsum.photos/100/100",
-      title: "플레이리스트2",
-      track_count: 10,
-    },
-    {
-      id: 3,
-      thumbnails: "https://picsum.photos/100/100",
-      title: "플레이리스트3",
-      track_count: 10,
-    },
-    {
-      id: 4,
-      thumbnails: "https://picsum.photos/100/100",
-      title: "플레이리스트4",
-      track_count: 10,
-    },
-    {
-      id: 5,
-      thumbnails: "https://picsum.photos/100/100",
-      title: "플레이리스트5",
-      track_count: 10,
-    },
-  ];
+  const { getMyPlaylistsQuery } = usePlaylist();
+  const debouncedLoading = useDebounce(getMyPlaylistsQuery.isLoading, 1000);
 
   return (
     <TopBarLayout
@@ -46,10 +18,14 @@ export default function PlaylistPage() {
         rightActionElement: <CreatePlaylistButton />,
       }}
     >
-      <section className="grid justify-between grid-cols-2 px-5 py-6 overflow-y-auto gap-x-4 gap-y-8">
-        {mock.map((item) => (
-          <PlaylistItem key={item.id} item={item} />
-        ))}
+      <section className="grid justify-between grid-cols-2 px-5 py-6 overflow-y-auto gap-x-4 gap-y-4">
+        {debouncedLoading ? (
+          <PlaylistSkeleton />
+        ) : (
+          getMyPlaylistsQuery.data?.map((item) => (
+            <PlaylistItem key={item.userPlaylistId} item={item} />
+          ))
+        )}
       </section>
     </TopBarLayout>
   );

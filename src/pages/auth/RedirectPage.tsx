56fdee807c4b7d76dafe58b28a9066e2458a3eeb
JIHU96 <korjihu@gmail.com>
2025-01-16 15:19:27 +0900

    fix: 유저 정보 조회 API 엔드포인트 수정

--- src/pages/auth/RedirectPage.tsx
+++ src/pages/auth/RedirectPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import styled from "styled-components";
 import LogoIcon from "@/assets/svg/logo.svg";
 import TextLogoIcon from "@/assets/svg/text-logo.svg";
@@ -11,20 +11,20 @@ import { instance } from "@/services/api/instance";
 const queryClient = new QueryClient();
 
 // API 호출 함수 (유저 정보 가져오기)
-const fetchUserData = async (accessToken: string) => {
-  const response = await instance.get("/users/1", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-  });
-  return response.data;
+const fetchUserData = async () => {
+  try {
+    const response = await instance.get("/user/profile");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // 유저 정보를 가져오는 React Query 훅
 function useUserData(accessToken: string) {
   return useQuery({
     queryKey: ["userData", accessToken],
-    queryFn: () => fetchUserData(accessToken),
+    queryFn: () => fetchUserData(),
     enabled: !!accessToken, // accessToken이 있을 때만 쿼리 실행
   });
 }

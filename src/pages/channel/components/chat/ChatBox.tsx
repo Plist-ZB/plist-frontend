51b37168d22b20b1eb69b2db42d732f99f0ba897
You-NaSong <twins7534@naver.com>
2025-01-18 02:05:50 +0900

    feat : 웹소켓 채팅 API 로직 구현/연결
    1. 웹소켓 연결 설정
    -클라이언트에서 웹소켓 서버에 연결
    -서버에서 수신한 메시지를 채팅 목록에 추가
    
    2. 메시지 전송 기능 구현
    -입력 필드를 통해 메시지를 작성하고 서버로 전송
    
    3.실시간 업데이트
    -서버에서 받은 메시지를 chats 상태에 업데이트

--- src/pages/channel/components/chat/ChatBox.tsx
+++ src/pages/channel/components/chat/ChatBox.tsx
@@ -1,15 +1,16 @@
 interface ChatBoxProps {
-  thumbnail: string;
+  thumbnail: string; // 기본 썸네일
   message: string;
   username: string;
+  userProfileImg?: string; // 새로운 프로필 이미지 (선택적)
 }
 
-export default function ChatBox({ thumbnail, message, username }: ChatBoxProps) {
+export default function ChatBox({ thumbnail, message, username, userProfileImg }: ChatBoxProps) {
   return (
     <div className="flex gap-3 p-4">
       <div
         className="w-10 h-10 bg-cover rounded-full bg-primary-500 shrink-0 aspect-square"
-        style={{ backgroundImage: `url('${thumbnail}')` }}
+        style={{ backgroundImage: `url('${userProfileImg || thumbnail}')` }} // userProfileImg가 있으면 해당 이미지를 사용
       ></div>
       <div>
         <div>{username}</div>

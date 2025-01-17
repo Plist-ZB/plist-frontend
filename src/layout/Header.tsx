2d6570aa12c6dce7243337aa3ff431c64631ffc6
JIHU96 <korjihu@gmail.com>
2025-01-17 16:11:31 +0900

    feat: 헤더의 로고 버튼 클릭 시 홈("/")으로 이동

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import Logo from "@/assets/svg/logo.svg";
 import TextLogo from "@/assets/svg/text-logo.svg";
 import HeaderButton from "@/layout/HeaderButton";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 interface HeaderProps {
   readonly className?: string;
@@ -14,10 +14,10 @@ export default function Header({ className }: HeaderProps) {
     <header
       className={`flex-shrink-0 py-0 pl-2 pr-4 h-header border-b border-[#D9D9D9] w-full bg-white flex items-center justify-between ${className}`}
     >
-      <div className="flex items-center">
+      <Link to="/" className="flex items-center">
         <Logo className="w-12 h-12 text-primary-main" />
         <TextLogo className="" />
-      </div>
+      </Link>
 
       {pathname !== "/mypage" && <HeaderButton />}
     </header>

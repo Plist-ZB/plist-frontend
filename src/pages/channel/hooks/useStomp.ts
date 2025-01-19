a7526f30ce2410137f77539cd37d275344c3e0c4
JIHU96 <korjihu@gmail.com>
2025-01-20 07:34:00 +0900

    refactor: useStomp 훅 로직 수정 => stompClient 관련만 정의

--- src/pages/channel/hooks/useStomp.ts
+++ src/pages/channel/hooks/useStomp.ts
@@ -1,57 +1,18 @@
 import { useState, useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import createStompClient from "@/pages/channel/utils/createStompClient";
 import { Client } from "@stomp/stompjs";
-import { instance } from "@/services/api/instance";
 
 export const useStomp = () => {
   const { channelId } = useParams();
-  const { state } = useLocation();
   const [stompClient, setStompClient] = useState<Client | undefined>(undefined);
-  const [isHost, setIsHost] = useState(false);
-  const [channelInfo, setChannelInfo] = useState<any>(null);
-
-  useEffect(() => {
-    const getChannelInfo = async () => {
-      const { data: response } = await instance.patch(`/channel/${channelId}`);
-
-      console.log("채널 입장", response);
-      setChannelInfo(response);
-    };
-
-    const exitChannel = async () => {
-      const response = await instance.patch(`/channel/exit/${channelId}`);
-
-      console.log("채널 나가기", response);
-    };
-
-    if (channelId) {
-      getChannelInfo();
-    }
-
-    return () => {
-      // exitChannel();
-    };
-  }, [channelId]);
-
-  useEffect(() => {
-    if (state) {
-      setIsHost(state.host);
-    }
-  }, [state]);
 
   useEffect(() => {
     if (!channelId) return;
 
     const client = createStompClient({
-      onConnectCallback: (client) => {
-        // console.log("연결 성공");
-        // client.subscribe(`/sub/chat.${channelId}`, function (message) {
-        //   // 메시지를 JSON 형식으로 파싱
-        //   console.log("SUB 채팅");
-        //   const chatMessage = JSON.parse(message.body);
-        //   console.log(chatMessage);
-        // });
+      onConnectCallback: (/* client */) => {
+        console.log("연결 성공");
       },
       onErrorCallback: (error) => {
         console.error("STOMP Connection Error: ", error);
@@ -66,9 +27,5 @@ export const useStomp = () => {
     };
   }, [channelId]);
 
-  return {
-    channelId,
-    stompClient,
-    channelInfo,
-  };
+  return { stompClient };
 };

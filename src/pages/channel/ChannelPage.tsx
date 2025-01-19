e6e8883dda976a6e05e32228e8827180621fa233
JIHU96 <korjihu@gmail.com>
2025-01-20 07:34:58 +0900

    refactor: stompClient 재정의, 테스트 버튼 제거

--- src/pages/channel/ChannelPage.tsx
+++ src/pages/channel/ChannelPage.tsx
@@ -4,16 +4,14 @@ import VideoPlayer from "@/pages/channel/components/VideoPlayer";
 import Playlist from "@/pages/channel/components/Playlist";
 import ChatArea from "@/pages/channel/components/ChatArea";
 import { useStomp } from "@/pages/channel/hooks/useStomp";
-import useChannelEvents from "@/pages/channel/hooks/useChannelEvents";
 import useGetChannelInfo from "@/pages/channel/hooks/useGetChannelInfo";
 
 export default function ChannelPage() {
   const getChannelInfoQuery = useGetChannelInfo();
+  const { stompClient } = useStomp();
 
   /* TODO: 위에서 얻은 id로 채널방 정보 가져오고 웹소켓 연결하기 */
   const [videoId, setVideoId] = useState("2g811Eo7K8U");
-
-  const stompClient = undefined;
 
   if (getChannelInfoQuery.isFetching) {
     return <div>Loading...</div>;
@@ -21,9 +19,9 @@ export default function ChannelPage() {
 
   console.log(getChannelInfoQuery.data);
 
-  /* if (stompClient) {
+  if (!stompClient) {
     return <div>Loading...</div>;
-  } */
+  }
 
   return (
     <div className="flex flex-col w-full h-screen overflow-hidden">
@@ -36,38 +34,6 @@ export default function ChannelPage() {
       )}
 
       <VideoPlayer videoId={videoId} stompClient={stompClient} />
-      {/* 테스트용 버튼 */}
-      <button
-        onClick={() => {
-          const pubMessage = {
-            email: "korjihu@gmail.com",
-            message: "테스트",
-          };
-
-          stompClient?.publish({
-            destination: `/pub/chat.${channelId}`,
-            body: JSON.stringify(pubMessage),
-          });
-        }}
-      >
-        메세지 전송 테스트
-      </button>
-      <button
-        onClick={() => {
-          const video = {
-            videoId: "string",
-            currentTime: 0,
-            playState: 0,
-          };
-
-          stompClient?.publish({
-            destination: `/pub/video.${channelId}`,
-            body: JSON.stringify(video),
-          });
-        }}
-      >
-        비디오 정보 전송 테스트
-      </button>
 
       <div className="flex flex-col flex-1 w-full min-h-0 bg-white">
         <Playlist isHost={getChannelInfoQuery.data.host} stompClient={stompClient} />

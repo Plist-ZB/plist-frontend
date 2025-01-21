fabd70f922cf5d81b2d632135fd62e6469c536a5
JIHU96 <korjihu@gmail.com>
2025-01-21 20:37:56 +0900

    feat: 현재 비디오 id를 저장하는 jotai state 추가

--- src/store/channel.ts
+++ src/store/channel.ts
@@ -5,3 +5,5 @@ export const channelAtom = atom<IChannel | null>(null);
 export const isChannelHostAtom = atom<boolean | null>(null);
 
 export const channelVideoListAtom = atom<IVideo[] | null>(null);
+
+export const currentVideoIdAtom = atom<string | null>(null);

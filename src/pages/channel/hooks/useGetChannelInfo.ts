09030b8a9971f7aaa617d9e47d1c399e38dbfbce
JIHU96 <korjihu@gmail.com>
2025-01-21 15:03:36 +0900

    feat: 채널 정보 조회 후 jotai state에 isHost와 비디오 리스트 저장

--- src/pages/channel/hooks/useGetChannelInfo.ts
+++ src/pages/channel/hooks/useGetChannelInfo.ts
@@ -1,14 +1,22 @@
 import { instance } from "@/services/api/instance";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { useSetAtom } from "jotai";
+import { isChannelHostAtom, channelVideoListAtom } from "@/store/channel";
 
 export default function useGetChannelInfo() {
   const { channelId } = useParams();
+  const setIsHost = useSetAtom(isChannelHostAtom);
+  const setChannelVideoList = useSetAtom(channelVideoListAtom);
 
   const getChannelInfoQuery = useQuery({
     queryKey: ["channelInfo", channelId],
     queryFn: async () => {
-      const { data: response } = await instance.get(`/channel/${channelId}`);
+      const { data: response } = await instance.get<IChannelData>(`/channel/${channelId}`);
+
+      console.log("in hook", response);
+      setIsHost(response.host);
+      setChannelVideoList(response.videoList);
 
       return response;
     },

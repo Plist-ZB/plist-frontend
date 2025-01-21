27d357b9e12ad758b8afa1ccec3a86f40aaf3c8f
JIHU96 <korjihu@gmail.com>
2025-01-21 18:53:32 +0900

    fix: 의도적으로 3초후에 이동하게 설정

--- src/pages/auth/RedirectPage.tsx
+++ src/pages/auth/RedirectPage.tsx
@@ -30,43 +30,39 @@ export default function RedirectPage() {
   useEffect(() => {
     // 유저 데이터가 성공적으로 로드된 후 페이지 이동
     if (user) {
-      if (isMember === "true") {
-        navigate("/"); // 홈페이지로 이동
-      } else {
-        navigate("/auth/welcome"); // Welcome 페이지로 이동
-      }
+      setTimeout(() => {
+        if (isMember === "true") {
+          navigate("/"); // 홈페이지로 이동
+        } else {
+          navigate("/auth/welcome"); // Welcome 페이지로 이동
+        }
+      }, 3000); // 1초 후에 이동
     }
   }, [user, isMember, navigate]);
-
-  // 로딩 상태 처리
-  if (isLoading) {
-    return (
-      <Container>
-        <LogoWrapper>
-          <LogoIcon />
-          <TextLogoIcon />
-        </LogoWrapper>
-        <Spinners>
-          <ScaleLoader
-            color="#4654A3"
-            height={40}
-            margin={2}
-            radius={0}
-            speedMultiplier={0.7}
-            width={6}
-          />
-        </Spinners>
-      </Container>
-    );
-  }
 
   // 에러 처리
   if (isError) {
     alert("사용자 정보를 가져오는 데 실패했습니다. 다시 시도해주세요.");
-    return null;
   }
 
-  return null;
+  return (
+    <Container>
+      <LogoWrapper>
+        <LogoIcon />
+        <TextLogoIcon />
+      </LogoWrapper>
+      <Spinners>
+        <ScaleLoader
+          color="#4654A3"
+          height={40}
+          margin={2}
+          radius={0}
+          speedMultiplier={0.7}
+          width={6}
+        />
+      </Spinners>
+    </Container>
+  );
 }
 
 const Container = styled.div`

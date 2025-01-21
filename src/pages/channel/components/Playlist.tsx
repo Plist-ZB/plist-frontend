e941d49a87607b47af2fec4e350dea42d457ed58
JIHU96 <korjihu@gmail.com>
2025-01-21 19:39:58 +0900

    fix: 채널 재생목록 페이지 수정
    - isHost 가져오기
    - 재생목록 리스트를 목데이터에서 페칭된 실제 데이터로 변경
    - 재생목록 드랍다운 on/off와 상관없이 현재 음악 보여주게 변경
    - isHost에 따라 재생목록 아이템 컴포넌트 분기

--- src/pages/channel/components/Playlist.tsx
+++ src/pages/channel/components/Playlist.tsx
@@ -1,28 +1,20 @@
 import { useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
 import PlayListItemBox from "@/pages/channel/components/playlist/PlayListItemBox";
+import HostPlayListItemBox from "@/pages/channel/components/playlist/HostPlayListItemBox";
 import { Client } from "@stomp/stompjs";
+import { useAtomValue } from "jotai";
+import { channelVideoListAtom, isChannelHostAtom } from "@/store/channel";
 
 interface PlaylistProps {
-  isHost: boolean;
   stompClient: Client;
 }
 
-const Playlist = ({ isHost, stompClient }: PlaylistProps) => {
-  const [playlist, setPlaylist] = useState<any[]>([
-    { id: 1, title: "먼데이키즈 - 봄 안부" },
-    { id: 2, title: "먼데이키즈 - 여름 안부" },
-    { id: 3, title: "먼데이키즈 - 가을 안부" },
-    { id: 4, title: "먼데이키즈 - 겨울 안부" },
-    { id: 5, title: "먼데이키즈 - 봄 안부" },
-    { id: 6, title: "먼데이키즈 - 여름 안부" },
-    { id: 7, title: "먼데이키즈 - 가을 안부" },
-    { id: 8, title: "먼데이키즈 - 겨울 안부" },
-    { id: 9, title: "먼데이키즈 - 봄 안부" },
-    { id: 10, title: "먼데이키즈 - 여름 안부" },
-    { id: 11, title: "먼데이키즈 - 가을 안부" },
-    { id: 12, title: "먼데이키즈 - 겨울 안부" },
-  ]);
+const Playlist = ({ stompClient }: PlaylistProps) => {
+  const isHost = useAtomValue(isChannelHostAtom);
+  const channelVideoList = useAtomValue(channelVideoListAtom);
+
+  console.log("in Playlist", isHost, channelVideoList);
 
   const [currentVideoId, setCurrentVideoId] = useState(2);
   const [isOpen, setIsOpen] = useState(false);
@@ -30,22 +22,18 @@ const Playlist = ({ isHost, stompClient }: PlaylistProps) => {
   return (
     <div className="relative">
       <div
-        className={`flex items-center justify-between gap-4 p-2 border-border ${
+        className={`flex items-center justify-between gap-2 p-2 border-border ${
           !isOpen ? "border-b" : ""
         }`}
       >
-        {!isOpen && (
-          <>
-            <div>현재 음악</div>
-            <div className="flex-1 text-base">
-              {playlist.find((item) => item.id === currentVideoId)?.title}
-            </div>
-          </>
-        )}
+        <div className="text-base font-semibold">현재 음악</div>
+        <div className="flex-1 text-base truncate">
+          {channelVideoList?.find((item) => item.id === currentVideoId)?.videoName}
+        </div>
 
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="flex items-center ml-auto font-medium text-black"
+          className="flex items-center p-2 pr-1 ml-auto font-medium text-black"
         >
           재생목록
           <IoMdArrowDropdown className="text-2xl" />
@@ -57,15 +45,25 @@ const Playlist = ({ isHost, stompClient }: PlaylistProps) => {
         /* TODO: 호스트가 아이템박스 클릭 시 현재 재생 영상 변경 */
         /* TODO: 클릭하면 favorite에 추가하기 */
         <div className="overflow-y-auto flex flex-col gap-2 px-2 py-4 absolute left-0 right-0 z-10 transition-all duration-300 ease-in-out origin-top transform bg-white border-b shadow-lg max-h-[calc(100vh*0.5)] top-full animate-dropdown rounded-b-lg">
-          {playlist.map((item) => (
-            <PlayListItemBox
-              key={item.id}
-              item={item}
-              currentVideoId={currentVideoId}
-              setCurrentVideoId={setCurrentVideoId}
-              setIsOpen={setIsOpen}
-            />
-          ))}
+          {channelVideoList?.map((item) =>
+            isHost ? (
+              <HostPlayListItemBox
+                key={item.id}
+                item={item}
+                currentVideoId={currentVideoId}
+                setCurrentVideoId={setCurrentVideoId}
+                setIsOpen={setIsOpen}
+              />
+            ) : (
+              <PlayListItemBox
+                key={item.id}
+                item={item}
+                currentVideoId={currentVideoId}
+                setCurrentVideoId={setCurrentVideoId}
+                setIsOpen={setIsOpen}
+              />
+            )
+          )}
         </div>
       )}
     </div>

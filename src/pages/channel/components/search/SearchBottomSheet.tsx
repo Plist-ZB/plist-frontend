4ba276394a7568523f48998adf9fd6381f8d5cee
JIHU96 <korjihu@gmail.com>
2025-01-22 04:32:02 +0900

    type: searchYoutubeQuery의 dat에 타입 부여

--- src/pages/channel/components/search/SearchBottomSheet.tsx
+++ src/pages/channel/components/search/SearchBottomSheet.tsx
@@ -20,7 +20,7 @@ export default function SearchBottomSheet({ isOpen, unmount, channelId }: Search
   const searchYoutubeQuery = useQuery({
     queryKey: ["searchVideo", search],
     queryFn: async () => {
-      const { data } = await instance.get(`/search-video?keyword=${search}`);
+      const { data } = await instance.get<IVideo[]>(`/search-video?keyword=${search}`);
       return data;
     },
     enabled: isSearchEnabled && search.length > 0,

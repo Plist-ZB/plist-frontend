5ac2bf17d2b7a3d6d1f25015684f2637e75cbeda
JIHU96 <korjihu@gmail.com>
2025-02-25 03:06:54 +0900

    fix: getEmailFromToken 함수의 반환값에 string 명시

--- src/pages/channel/utils/getDataFromToken.ts
+++ src/pages/channel/utils/getDataFromToken.ts
@@ -3,5 +3,5 @@ export const getEmailFromToken = () => {
   const payload = accessToken?.split(".")[1] as string;
   const decodedPayload = JSON.parse(atob(payload));
 
-  return decodedPayload.email;
+  return decodedPayload.email as string;
 };

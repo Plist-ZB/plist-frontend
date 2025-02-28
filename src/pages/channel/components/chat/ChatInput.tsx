b76883b00c51843941d7b5408039fef95631363d
JIHU96 <korjihu@gmail.com>
2025-02-28 19:52:34 +0900

    fix: 한글 채팅 입력 시 onKeyDown 이벤트 2번 발생하는 이슈 수정
    - input 이벤트 타입도 추가

--- src/pages/channel/components/chat/ChatInput.tsx
+++ src/pages/channel/components/chat/ChatInput.tsx
@@ -3,6 +3,7 @@ import { overlay } from "overlay-kit";
 import { useState } from "react";
 import SearchBottomSheet from "../search/SearchBottomSheet";
 import { useParams } from "react-router-dom";
+import { InputChangeEvent } from "@/types/event";
 
 interface ChatInputProps {
   onSendMessage: (message: string) => void;
@@ -17,22 +18,31 @@ const openSearchBottomSheet = (channelId: string) => () => {
 export default function ChatInput({ onSendMessage }: ChatInputProps) {
   const { channelId } = useParams();
   const [message, setMessage] = useState("");
+  const [isComposing, setIsComposing] = useState(false);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: InputChangeEvent) => {
     setMessage(e.target.value);
   };
 
   const sendMessage = () => {
-    if (message.trim()) {
+    if (message.trim() && !isComposing) {
       onSendMessage(message); // 부모로 메시지 전달
       setMessage(""); // 입력 필드 초기화
     }
   };
 
   const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
+    if (e.key === "Enter" && !isComposing) {
       sendMessage();
     }
+  };
+
+  const handleCompositionStart = () => {
+    setIsComposing(true);
+  };
+
+  const handleCompositionEnd = () => {
+    setIsComposing(false);
   };
 
   return (
@@ -47,6 +57,8 @@ export default function ChatInput({ onSendMessage }: ChatInputProps) {
         onChange={handleChange}
         className="flex-grow px-4 py-2 border rounded-lg border-border"
         onKeyDown={onKeyDown}
+        onCompositionStart={handleCompositionStart}
+        onCompositionEnd={handleCompositionEnd}
       />
       <button className="p-2" onClick={sendMessage}>
         <Send />

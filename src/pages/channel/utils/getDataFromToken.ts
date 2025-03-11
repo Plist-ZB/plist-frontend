7b39e5cd44daa0f476973abbb3c6be887ae245f7
JIHU96 <korjihu@gmail.com>
2025-03-12 01:12:29 +0900

    fix: accessToken이 없는 경우 null 값 반환하도록 수정

--- src/pages/channel/utils/getDataFromToken.ts
+++ src/pages/channel/utils/getDataFromToken.ts
@@ -1,6 +1,9 @@
 export const getEmailFromToken = () => {
   const accessToken = localStorage.getItem("access_token");
-  const payload = accessToken?.split(".")[1] as string;
+
+  if (!accessToken) return null;
+
+  const payload = accessToken.split(".")[1] as string;
   const decodedPayload = JSON.parse(atob(payload));
 
   return decodedPayload.email as string;

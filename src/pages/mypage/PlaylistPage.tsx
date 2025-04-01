50205332c3271bf0b532e1e3fc970f0568cf98c6
JIHU96 <korjihu@gmail.com>
2025-04-01 16:01:50 +0900

    feat: InfiniteScroll를 사용하여 내 전체 플레이리스트 페이지에 무한 스크롤 구현

--- src/pages/mypage/PlaylistPage.tsx
+++ src/pages/mypage/PlaylistPage.tsx
@@ -4,10 +4,16 @@ import PlaylistItem from "@/pages/mypage/components/playlist/PlaylistItem";
 import PlaylistSkeleton from "@/pages/mypage/components/playlist/PlaylistSkeleton";
 import usePlaylist from "@/pages/mypage/hooks/usePlaylist";
 import { useDebounce } from "@/common/hooks/useDebounce";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function PlaylistPage() {
-  const { getMyPlaylistsQuery } = usePlaylist();
-  const debouncedLoading = useDebounce(getMyPlaylistsQuery.isLoading, 1000);
+  const {
+    getMyPlaylistsQuery: { data, hasNextPage, isLoading, fetchNextPage },
+  } = usePlaylist();
+  const debouncedLoading = useDebounce(isLoading, 500);
+
+  const content = data?.pages.flatMap((page) => page.content) ?? [];
+  const dataLength = content.length;
 
   return (
     <TopBarLayout
@@ -18,15 +24,21 @@ export default function PlaylistPage() {
         rightActionElement: <CreatePlaylistButton />,
       }}
     >
-      <section className="grid justify-between grid-cols-2 px-5 py-6 overflow-y-auto gap-x-4 gap-y-4">
-        {debouncedLoading ? (
-          <PlaylistSkeleton />
-        ) : (
-          getMyPlaylistsQuery.data?.map((item) => (
-            <PlaylistItem key={item.userPlaylistId} item={item} />
-          ))
-        )}
-      </section>
+      <InfiniteScroll
+        dataLength={dataLength}
+        next={fetchNextPage}
+        hasMore={hasNextPage}
+        loader={<p className="p-1 text-center">플레이리스트를 가져오는 중입니다...</p>}
+        endMessage={<p className="text-center"></p>}
+      >
+        <section className="grid justify-between grid-cols-2 px-5 py-6 overflow-y-auto gap-x-4 gap-y-4">
+          {debouncedLoading ? (
+            <PlaylistSkeleton />
+          ) : (
+            content.map((item) => <PlaylistItem key={item.userPlaylistId} item={item} />)
+          )}
+        </section>
+      </InfiniteScroll>
     </TopBarLayout>
   );
 }

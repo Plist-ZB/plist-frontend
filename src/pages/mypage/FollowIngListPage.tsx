a094c2c110c210f045bea2667c1120cd75cb4309
JIHU96 <korjihu@gmail.com>
2025-04-09 17:33:06 +0900

    fix: tsc -b 결과 반영

--- src/pages/mypage/FollowIngListPage.tsx
+++ src/pages/mypage/FollowIngListPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function FollowIngListPage() {
   return <div>FollowIngListPage</div>;
 }
--- src/pages/user/UserPastStreamDetailPage.tsx
+++ src/pages/user/UserPastStreamDetailPage.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function UserPastStreamDetailPage() {
   return <div>UserPastStreamDetailPage</div>;
 }

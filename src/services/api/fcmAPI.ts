b0f33be49a67779b8c16134049c78d4748f3a20e
JIHU96 <korjihu@gmail.com>
2025-04-20 18:26:26 +0900

    fix: FCM API 함수명을 좀 더 직관적인 이름으로 변경

--- src/services/api/fcmAPI.ts
+++ src/services/api/fcmAPI.ts
@@ -1,12 +1,12 @@
 import { instance } from "@/services/api/instance";
 
-export const sendToken = async (fcmToken: string) => {
+export const sendTokenToServer = async (fcmToken: string) => {
   const response = await instance.post("/fcm/token", fcmToken);
 
   return response;
 };
 
-export const deleteToken = async (fcmToken: string) => {
+export const deleteTokenFromServer = async (fcmToken: string) => {
   const response = await instance.delete("/fcm/token", { data: fcmToken });
 
   return response;

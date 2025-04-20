20250e3448c1d03bbdefa70498c45ec901027534
JIHU96 <korjihu@gmail.com>
2025-04-20 18:40:06 +0900

    fix: 사용하지 않는 코드 제거 (tsc -b 결과 처리)

--- src/pages/mypage/FollowIngListPage.tsx
+++ src/pages/mypage/FollowIngListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import TopBarLayout from "@/layout/TopBarLayout";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";

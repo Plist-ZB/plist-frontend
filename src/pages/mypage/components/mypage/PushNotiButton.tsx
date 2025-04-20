ea2c2585f4d77a36b418e89ca34258348661f3c9
JIHU96 <korjihu@gmail.com>
2025-04-20 18:35:56 +0900

    feat: 후시 알림 설정 버튼에 FCM 토큰 정의/삭제하는 기능 추가
    - FCM 서버로부터 토큰을 정의/삭제하고 해당 기능을 수행한 결과를 API 백엔드 서버와 동기화하는 기능 구현
    - 로딩 중일 경우 로딩 컴포넌트를 화면에 렌더링 -> 로딩이 종료되면 로딩 컴포넌트를 unmount
    - 푸시 알림을 설정했을 경우 안읽은 메세지 조회 query를 무효화하여 재페칭하게 함 -> 위치: 루트 레이아웃의 헤더 컴포넌트의 알림 아이콘

--- src/pages/mypage/components/mypage/PushNotiButton.tsx
+++ src/pages/mypage/components/mypage/PushNotiButton.tsx
@@ -1,11 +1,27 @@
-import { useState } from "react";
+import usePushNoti from "@/pages/mypage/hooks/usePushNoti";
+import { overlay } from "overlay-kit";
+import { useEffect } from "react";
+import Loading from "@/common/components/Loading";
+
+const openLoadingOverlay = () => overlay.open(() => <Loading />, { overlayId: "loadingOverlay" });
+const unmountLoadingOverlay = () => overlay.unmount("loadingOverlay");
 
 export default function PushNotiButton() {
-  const [isPushAllowed, setIsPushAllowed] = useState(false);
+  const { isPushAllowed, onClickPushHandler, isOffPushLoading, isOnPushLoading } = usePushNoti();
+  const isLoading = isOffPushLoading || isOnPushLoading;
+
+  useEffect(() => {
+    if (isLoading) {
+      openLoadingOverlay();
+    }
+    return () => {
+      unmountLoadingOverlay();
+    };
+  }, [isLoading]);
 
   return (
     <button
-      onClick={() => setIsPushAllowed(!isPushAllowed)}
+      onClick={onClickPushHandler}
       className={`justify-start transition-all p-1 duration-300 flex items-center w-10 border-none rounded-2xl h-1/2 hover:text-inherit ${
         isPushAllowed ? "bg-primary-main pl-5" : "bg-gray-light"
       }`}
